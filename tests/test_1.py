3951e69815d61ea866b40ff4de9e25538df7f3e8
Iuliia Marutyak <iuliia.marutyak@globallogic.com>
2017-07-05 20:40:59 +0300

    Refactoring tests

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,11 +1,20 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-def test_google():
-    driver = webdriver.Chrome()
-    driver.get("http://www.google.com")
-    driver.find_element_by_name("q").send_keys("webdriver")
-    driver.find_element_by_name("btnG").click()
-    WebDriverWait(driver, 3).until(ec.title_is("webdriver - Пошук Google"))
-    driver.quit()
+def test_google_using_chrome():
+    pageAddr = "http://localhost/litecart/admin"
+    username = "admin"
+    password = "admin"
+
+    chrome_driver = webdriver.Chrome()
+    chrome_driver.get(pageAddr)
+    chrome_driver.find_element_by_name("username").send_keys(username)
+    chrome_driver.find_element_by_name("password").send_keys(password)
+    chrome_driver.find_element_by_name("login").submit()
+    WebDriverWait(chrome_driver, 3).until(ec.title_is("My Store"))
+    sleep(5)
+    chrome_driver.quit()
+
